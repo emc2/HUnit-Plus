30100fef0033fc03fbf6f93777165545294140fd
Eric McCorkle <emc2@metricspace.net>
2014-03-22 23:53:26 -0400

    Added skeleton files for Reporting tests

--- test/Tests/Test/HUnit.hs
+++ test/Tests/Test/HUnit.hs
@@ -5,9 +5,10 @@ import qualified Tests.Test.HUnit.Base as Base
 import qualified Tests.Test.HUnit.Execution as Execution
 import qualified Tests.Test.HUnit.Filter as Filter
 import qualified Tests.Test.HUnit.Main as Main
+import qualified Tests.Test.HUnit.Reporting as Reporting
 import qualified Tests.Test.HUnit.Text as Text
 import qualified Tests.Test.HUnit.XML as XML
 
 tests :: Test
 tests = testGroup "HUnit" [Base.tests, Execution.tests, Filter.tests,
-                           Main.tests, Text.tests, XML.tests]
+                           Reporting.tests, Main.tests, Text.tests, XML.tests]
---
+++ test/Tests/Test/HUnit/Reporting.hs
@@ -0,0 +1,7 @@
+module Tests.Test.HUnit.Reporting where
+
+import Distribution.TestSuite
+import Test.HUnit.Reporting
+
+tests :: Test
+tests = testGroup "Reporting" []
